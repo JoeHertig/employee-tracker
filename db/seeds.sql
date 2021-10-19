daec7cf55c3a9a9968d025fc8d40226e187cf553
Joe Hertig <joehertig94@gmail.com>
2021-10-19 15:46:19 -0600

    half way through queries

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,12 +8,12 @@ VALUES
 INSERT INTO roles (title, salary, department_id)
 VALUES
   ('Sales Lead', '100000', 1),
-  ('Salesperson', '80000', 2),
-  ('Lead Engineer', '150000', 3),
-  ('Software Engineer', '120000', 4),
-  ('Accountant', '125000', 5),
-  ('Legal Team Lead', '250000', 6),
-  ('Lawyer', '190000', 7);
+  ('Salesperson', '80000', 1),
+  ('Lead Engineer', '150000', 2),
+  ('Software Engineer', '120000', 2),
+  ('Accountant', '125000', 3),
+  ('Legal Team Lead', '250000', 4),
+  ('Lawyer', '190000', 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
@@ -23,5 +23,4 @@ VALUES
   ('Kevin', 'Tupik', 4, 3),
   ('Malia', 'Brown', 5, NULL),
   ('Sarah', 'Lourd', 6, NULL),
-  ('Tom', 'Allen', 7, 6),
-  ('Christian', 'Echenrode', 8, 2);
+  ('Tom', 'Allen', 7, 6);
--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const mysql = require("mysql2");
 const db = require("./db/connection");
 const cTable = require("console.table");
 
+// Text banner
 console.log(`
 .------------------------------------------------------.
 |     _____                 _                          |
 |    | ____|_ __ ___  _ __ | | ___  _   _ ____ ____    |
 |    |  _| |  _ ' _  | '_  | |/ _  | | | |  _ |  _ |   |
 |    | |___| | | | | | |_| | | |_| | |_| |  __/  __/   |
-|    |_____|_| |_| |_| .__/|_||___/| __, |____| ____|  |
+|    |_____|_| |_| |_| .__/|_||___/| __, |____|____|   |
 |                    |_|            |___/              |
 |                                                      |
 |     __  __                                           |
@@ -41,7 +42,6 @@ const startingPrompt = async () => {
       },
     ])
     .then((response) => {
-      console.log(response);
       switch (response.startingPrompt) {
         case "View All Departments":
           viewAllDepartments();
@@ -67,14 +67,71 @@ const startingPrompt = async () => {
       }
     });
 };
+// Starting prompt end
 
+// Queries
 function viewAllDepartments() {
-  db.query("SELECT * FROM departments;", function (err, res) {
+  db.query("SELECT * FROM departments", function (err, res) {
     if (err) throw err;
     console.table(res);
     startingPrompt();
   });
 }
+
+function viewAllRoles() {
+  db.query(
+    "SELECT roles.id, roles.title, departments.name As department, roles.salary FROM roles LEFT JOIN departments on roles.department_id = departments.id",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startingPrompt();
+    }
+  );
+}
+
+function viewAllEmployees() {
+  db.query(
+    "SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employees manager ON manager.id = employees.manager_id",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startingPrompt();
+    }
+  );
+}
+
+function addDepartment() {
+  db.query("SELECT * FROM departments", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    startingPrompt();
+  });
+}
+
+function addRole() {
+  db.query("SELECT * FROM departments", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    startingPrompt();
+  });
+}
+
+function addEmployee() {
+  db.query("SELECT * FROM departments", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    startingPrompt();
+  });
+}
+
+function updateEmployeeRole() {
+  db.query("SELECT * FROM departments", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    startingPrompt();
+  });
+}
+//Queries End
 
 // start prompt
 startingPrompt();
