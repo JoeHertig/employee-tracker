1fabdf320428abb4d729e3b533016219c4335b55
Joe Hertig <joehertig94@gmail.com>
2021-10-20 18:23:39 -0600

    added code but errors happen on last 3 queries

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# employee-tracker
+# Employee Tracker
+
+## License
+
+This application is covered under the MIT license.
+
+[
+<img src="https://img.shields.io/static/v1?label=LICENSE&message=MIT&color=informational&style=for-the-badge" />
+](https://choosealicense.com/)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Testing](#testing)
+- [Questions](#questions)
+
+## Description
+
+An application where the user is able to view and manage the departments, roles, and employees in a company.
+
+## Installation
+
+To run this application, install the following:
+
+`npm i express`
+
+## Usage
+
+Create database and tables run in mysql:
+
+`source db/db.sql`
+`source db/schema.sql`
+`source db/seeds.sql`
+
+Then quit mysql command prompt and run:
+
+`node index`
+OR
+`node index.js`
+
+## Contribution Guidelines
+
+No Contributors
+
+## Testing
+
+N/A
+
+## Questions
+
+Link to my GitHub: [JoeHertig](https://github.com/JoeHertig).
+
+Email me for questions: [joehertig94@gmail.com](mailto:joehertig94@gmail.com).
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mysql = require("mysql2");
 const db = require("./db/connection");
 const cTable = require("console.table");
 
+var roleArr = [];
+var managersArr = [];
+
 // Text banner
 console.log(`
 .------------------------------------------------------.
@@ -71,7 +74,7 @@ const startingPrompt = async () => {
 
 // Queries
 function viewAllDepartments() {
-  db.query("SELECT * FROM departments", function (err, res) {
+  db.query("SELECT * FROM departments;", function (err, res) {
     if (err) throw err;
     console.table(res);
     startingPrompt();
@@ -80,7 +83,7 @@ function viewAllDepartments() {
 
 function viewAllRoles() {
   db.query(
-    "SELECT roles.id, roles.title, departments.name As department, roles.salary FROM roles LEFT JOIN departments on roles.department_id = departments.id",
+    "SELECT roles.id, roles.title, departments.name As department, roles.salary FROM roles LEFT JOIN departments on roles.department_id = departments.id;",
     function (err, res) {
       if (err) throw err;
       console.table(res);
@@ -91,7 +94,7 @@ function viewAllRoles() {
 
 function viewAllEmployees() {
   db.query(
-    "SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employees manager ON manager.id = employees.manager_id",
+    "SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employees manager ON manager.id = employees.manager_id;",
     function (err, res) {
       if (err) throw err;
       console.table(res);
@@ -101,35 +104,189 @@ function viewAllEmployees() {
 }
 
 function addDepartment() {
-  db.query("SELECT * FROM departments", function (err, res) {
-    if (err) throw err;
-    console.table(res);
-    startingPrompt();
-  });
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "departmentName",
+        message: "What is the Department name?",
+      },
+    ])
+    .then((response) => {
+      var query = db.query(
+        "INSERT INTO departments SET ? ;",
+        {
+          name: response.departmentName,
+        },
+        function (err) {
+          if (err) throw err;
+          console.table(response);
+          startingPrompt();
+        }
+      );
+    });
 }
 
 function addRole() {
-  db.query("SELECT * FROM departments", function (err, res) {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleName",
+        message: "What is the Role's Title?",
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "What is the Salary?",
+      },
+      //   {
+      //     type: "input",
+      //     name: "roleDepartment",
+      //     message: "What is the Role's Department?",
+      //   },
+    ])
+    .then((response) => {
+      db.query(
+        "SELECT roles.title, roles.salary FROM roles",
+        {
+          title: response.roleName,
+          salary: response.roleSalary,
+        },
+        function (err) {
+          if (err) throw err;
+          console.table(response);
+          startingPrompt();
+        }
+      );
+      //   db.query(
+      //     "INSERT INTO departments SET ? ",
+      //     {
+      //       name: response.roleDepartment,
+      //     },
+      //     function (err) {
+      //       if (err) throw err;
+      //       console.table(response);
+      //       startingPrompt();
+      //     }
+      //   );
+    });
+}
+
+function selectRole() {
+  db.query("SELECT * FROM roles;", function (err, res) {
     if (err) throw err;
-    console.table(res);
-    startingPrompt();
+    for (var i = 0; i < res.length; i++) {
+      roleArr.push(res[i].title);
+    }
   });
+  return roleArr;
+}
+
+function selectManager() {
+  db.query(
+    "SELECT first_name, last_name FROM employees WHERE manager_id IS NULL;",
+    function (err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        managersArr.push(res[i].first_name);
+      }
+    }
+  );
+  return managersArr;
 }
 
 function addEmployee() {
-  db.query("SELECT * FROM departments", function (err, res) {
-    if (err) throw err;
-    console.table(res);
-    startingPrompt();
-  });
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "employeeName",
+        message: "What is the Employee's first name?",
+      },
+      {
+        type: "input",
+        name: "employeeLastName",
+        message: "What is the Employee's last name?",
+      },
+      {
+        type: "list",
+        name: "employeeRole",
+        message: "What is the Employee's Role?",
+        choices: selectRole(),
+      },
+      {
+        type: "rawlist",
+        name: "employeeManager",
+        message: "Who is the Employee's Manager?",
+        choices: selectManager(),
+      },
+    ])
+    .then(function (val) {
+      var roleId = selectRole().indexOf(val.role) + 1;
+      var managerId = selectManager().indexOf(val.choice) + 1;
+      db.query(
+        "INSERT INTO employees SET ? ",
+        {
+          first_name: val.employeeName,
+          last_name: val.employeeLastName,
+          manager_id: managerId,
+          role_id: roleId,
+        },
+        function (err) {
+          if (err) throw err;
+          console.table(val);
+          startingPrompt();
+        }
+      );
+    });
 }
 
 function updateEmployeeRole() {
-  db.query("SELECT * FROM departments", function (err, res) {
-    if (err) throw err;
-    console.table(res);
-    startingPrompt();
-  });
+  db.query(
+    "SELECT employees.last_name, roles.title FROM employees JOIN roles ON employees.role_id = roles.id;",
+    function (err, res) {
+      if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            type: "rawlist",
+            name: "lastName",
+            choices: function () {
+              var lastName = [];
+              for (var i = 0; i < res.length; i++) {
+                lastName.push(res[i].last_name);
+              }
+              return lastName;
+            },
+            message: "What is the Employees last name? ",
+          },
+          {
+            name: "role",
+            type: "rawlist",
+            message: "What is the Employees new title?",
+            choices: selectRole(),
+          },
+        ])
+        .then(function (val) {
+          var roleId = selectRole().indexOf(val.role) + 1;
+          db.query(
+            "UPDATE employees SET WHERE ?",
+            {
+              last_name: val.lastName,
+            },
+            {
+              role_id: roleId,
+            },
+            function (err) {
+              if (err) throw err;
+              console.table(val);
+              startingPrompt();
+            }
+          );
+        });
+    }
+  );
 }
 //Queries End
 
